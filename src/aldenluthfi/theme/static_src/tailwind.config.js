c1f7cb78c36ee314860b7a3fd5a42687e34e7064
aldenluthfi <aldenluthfi@gmail.com>
2024-12-26 18:47:13 +0700

    build: new server

--- src/aldenluthfi/theme/static_src/tailwind.config.js
+++ src/aldenluthfi/theme/static_src/tailwind.config.js
@@ -127,5 +127,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('tailwindcss-motion'),
     ],
 }
