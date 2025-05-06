f81f7834d83b529df72d893d6b0e2f6f006ad4cc
aldenluthfi <aldenluthfi@gmail.com>
2025-05-06 14:08:45 +0700

    port to react pt 0

--- src/start.sh
+++
@@ -1,8 +0,0 @@
-#!bin/sh
-python manage.py tailwind install --no-input
-python manage.py tailwind build --no-input
-
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-
-gunicorn aldenluthfi.wsgi:application --bind 0.0.0.0:8000 --workers 3 --threads 8 --timeout 300
