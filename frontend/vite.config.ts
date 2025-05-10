1008ca50ef4baf524d498564a79086107d3dac79
aldenluthfi <aldenluthfi@gmail.com>
2025-05-10 22:26:44 +0700

    trouble with env vars

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import path from "path"
+import dotenv from "dotenv"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
+dotenv.config()
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
