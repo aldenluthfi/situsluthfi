2f8a4ea0a30a9062039c09fee5a1cfa630246348
aldenluthfi <aldenluthfi@gmail.com>
2025-06-02 11:58:06 +0700

    daily build

--- frontend/src/components/ui/carousel.tsx
+++ frontend/src/components/ui/carousel.tsx
@@ -174,7 +174,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
 
 function CarouselPrevious({
     className,
-    variant = "outline",
+    variant = "ghost",
     size = "icon",
     ...props
 }: React.ComponentProps<typeof Button>) {
@@ -186,7 +186,7 @@ function CarouselPrevious({
             variant={variant}
             size={size}
             className={cn(
-                "absolute size-8 rounded-md",
+                "absolute text-primary hover:bg-background",
                 orientation === "horizontal"
                     ? "top-1/2 -left-12 -translate-y-1/2"
                     : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -204,7 +204,7 @@ function CarouselPrevious({
 
 function CarouselNext({
     className,
-    variant = "outline",
+    variant = "ghost",
     size = "icon",
     ...props
 }: React.ComponentProps<typeof Button>) {
@@ -216,7 +216,7 @@ function CarouselNext({
             variant={variant}
             size={size}
             className={cn(
-                "absolute size-8 rounded-md",
+                "absolute text-primary hover:bg-background",
                 orientation === "horizontal"
                     ? "top-1/2 -right-12 -translate-y-1/2"
                     : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
