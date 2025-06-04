73be8edc1be837d9950dcc0825d74abdc2863e1d
aldenluthfi <aldenluthfi@gmail.com>
2025-06-05 00:56:10 +0700

    maps

--- frontend/src/pages/Gallery.tsx
+++ frontend/src/pages/Gallery.tsx
@@ -1,4 +1,6 @@
 import SlidingTitle from '../components/custom/sliding-title';
+import World from "@react-map/world"
+import Indonesia from "@react-map/indonesia";
 
 const Gallery: React.FC = () => {
     return (
@@ -16,6 +18,35 @@ const Gallery: React.FC = () => {
                     of my travels around this <span className="text-primary font-body-bold">blob of dust</span>
                 </p>
             </div>
+            <div className='flex justify-center items-center'>
+                <World
+                    type='select-multiple'
+                    hoverColor="var(--primary)"
+                    selectColor="var(--primary)"
+                    mapColor='var(--muted)'
+                    strokeColor='var(--muted-foreground)'
+                    strokeWidth={0.75}
+                    hints
+                    hintBackgroundColor='var(--primary-200)'
+                    hintTextColor='var(--primary-700)'
+                    size={window.innerWidth < 768 ? 400 : window.innerWidth < 1024 ? 800 : window.innerWidth < 1536 ? 1200 : 1600}
+                />
+            </div>
+            <div className='flex justify-center items-center'>
+                <Indonesia
+                    type='select-multiple'
+                    hoverColor="var(--primary)"
+                    selectColor="var(--primary)"
+                    mapColor='var(--muted)'
+                    strokeColor='var(--muted-foreground)'
+                    strokeWidth={0.75}
+                    hints
+                    hintBackgroundColor='var(--primary-200)'
+                    hintTextColor='var(--primary-700)'
+                    size={window.innerWidth < 768 ? 400 : window.innerWidth < 1024 ? 800 : window.innerWidth < 1536 ? 1200 : 1600}
+                />
+            </div>
+
         </div>
     );
 };
