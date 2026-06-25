0fdfe45aec561133ce595005afdb04ddc92927f1
aldenluthfi <aldenluthfi@gmail.com>
2026-06-25 23:49:10 +0700

    refactor: linting

--- backend/src/services/facts_service.ts
+++ backend/src/services/facts_service.ts
@@ -1,4 +1,4 @@
-import { staticFacts } from "../lib/facts_data";
+import { staticFacts } from "../lib/facts";
 
 export const getRandomFactService = async () => {
     const randomIndex = Math.floor(Math.random() * staticFacts.length);
